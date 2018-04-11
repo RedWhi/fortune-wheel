4d345fbfd5c300c24c6fd50d410de0631b250c20
Viktor <walkman2591@gmail.com>
2018-04-12 00:51:38 +0300

    add project

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+﻿window.onload = function() {
+	 wheel = document.getElementById("circle");
+     button = document.getElementById("button");
+     win = document.getElementById("win");
+	
+}
+var my_timer = function () {
+  	win.style.display = "block";
+  }
+var rando = 1260;
+
+
+var spin_wheel = function () {
+
+  wheel.style.webkitTransform = "rotate(" + rando + "deg)";
+  wheel.style.mozTransform = "rotate(" + rando + "deg)";
+  wheel.style.msTransform = "rotate(" + rando + "deg)";
+  wheel.style.transform = "rotate(" + rando + "deg)";
+  
+  setTimeout (my_timer, 10000);  
+}
